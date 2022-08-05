9ce899f460332099f8daac34d2729b735d4dd666
jhonathan <jhonathancruz48@gmail.com>
2022-08-05 09:22:56 -0300

    primeiro componente, criação de função e mudança de comportamento

--- src/App.js
+++ src/App.js
@@ -21,12 +21,39 @@ import React from 'react';
 //   );
 // }
 
-function App() {
-  return (
-    <div >
-        Hello World!
-    </div>
-  );
+class App extends React.Component{
+  
+  state = {
+    numero1 : '',
+    numero2 : '',
+    resultado : ''
+  }
+
+  somar = () => {
+    const resultado = parseInt(this.state.numero1) + parseInt(this.state.numero2)
+    this.setState({resultao : resultado})
+    
+  }
+
+  render(){
+    return(
+      <div>
+        <label>Número 1:</label>
+        <input type="text" value={this.state.numero1} 
+               onChange={(e) => this.setState({numero1: e.target.value})}></input>
+        <br/>
+        <label>Número 2:</label>
+        <input type="text" value={this.state.numero2} 
+               onChange={(e) => this.setState({numero2: e.target.value})}></input>
+
+        <br/>
+        <button onClick={this.somar}>Somar</button>
+
+        O resultado é: {this.state.resultado}
+      </div>
+    )
+  }
+
 }
 
 export default App;
