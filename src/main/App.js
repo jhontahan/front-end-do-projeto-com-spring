9189452142cb1183ffd4dd3502babca52f7c6419
jhonathan <jhonathancruz48@gmail.com>
2022-08-10 17:11:21 -0300

    importando o primereact no projeto

--- src/main/App.js
+++ src/main/App.js
@@ -11,6 +11,11 @@ import "bootswatch/dist/flatly/bootstrap.css";
 import ".././custom.css";
 import "toastr/build/toastr.css"
 
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";
+
+
 // function App() {
 //   return (
 //     <div className="App">
